fee6ae3e05fd25ad0a31d45c625788203894e4c7
Bach Le <bach@bullno1.com>
2024-11-12 00:58:14 +0800

    bhash: Reduce macro usage

--- tests/bhash/main.c
+++ tests/bhash/main.c
@@ -1,8 +1,10 @@
+#define BLIB_IMPLEMENTATION
 #include "../../bhash.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <assert.h>
 
-typedef BHASH_TABLE(int, int) table_t;
+typedef BHASH_TABLE(int, char) table_t;
 
 enum {
 	BHASH_TEST_ADD,
@@ -15,19 +17,14 @@ int main(int argc, const char* argv[]) {
 	(void)argc;
 	(void)argv;
 
-	bhash_config_t config = {
-		.eq = bhash_eq,
-		.hash = bhash_hash,
-		.has_values = true,
-		.removable = true,
-	};
-	table_t* tbl = NULL;
+	table_t tbl;
+	bhash_init(&tbl, bhash_config_default());
 
 	// A simple boolean table to track membership of each number in [0, 10)
 	bool memberships[10] = { 0 };
 
 	for (int i = 0; i < 99999; ++i) {
-		printf("i = %d, len = %d\n", i, bhash_len(tbl));
+		printf("i = %d, len = %d\n", i, bhash_len(&tbl));
 
 		int action = rand() % BHASH_TEST_COUNT;
 		int key = rand() % 10;
@@ -36,66 +33,66 @@ int main(int argc, const char* argv[]) {
 		(void)index;
 
 		if (action == BHASH_TEST_ADD) {
-			bhash_find(index, tbl, key, config);
-			bhash_index_t len_before = bhash_len(tbl);
+			index = bhash_find(&tbl, key);
+			bhash_index_t len_before = bhash_len(&tbl);
 			bool existed = bhash_is_valid(index);
 
 			printf("Add %d -> %d\n", key, key * 2);
-			bhash_put(tbl, key, key * 2, config);
+			bhash_put(&tbl, key, (int){ key * 2 });
 			memberships[key] = true;
 
-			bhash_index_t len_after = bhash_len(tbl);
+			bhash_index_t len_after = bhash_len(&tbl);
 			if (existed) {
 				BHASH_ASSERT(len_after == len_before, "%s: %d -> %d", len_before, len_after);
 			} else {
 				BHASH_ASSERT(len_after == len_before + 1, "%s: %d -> %d", len_before, len_after);
 			}
 
-			bhash_find(index, tbl, key, config);
+			index = bhash_find(&tbl, key);
 
 			assert(bhash_is_valid(index));
-			BHASH_ASSERT(bhash_keys(tbl)[index] == key, "%s: %d vs %d", bhash_keys(tbl)[index], key);
-			BHASH_ASSERT(bhash_values(tbl)[index] == key * 2, "%s: %d vs %d", bhash_values(tbl)[index], key * 2);
+			BHASH_ASSERT(tbl.keys[index] == key, "%s: %d vs %d", tbl.keys[index], key);
+			BHASH_ASSERT(tbl.values[index] == key * 2, "%s: %d vs %d", tbl.values[index], key * 2);
 		} else if (action == BHASH_TEST_REMOVE) {
 			printf("Remove %d\n", key);
-			bhash_index_t len_before = bhash_len(tbl);
-			bhash_remove(index, tbl, key, config);
-			bhash_index_t len_after = bhash_len(tbl);
+			bhash_index_t len_before = bhash_len(&tbl);
+			index = bhash_remove(&tbl, key);
+			bhash_index_t len_after = bhash_len(&tbl);
 			memberships[key] = false;
 
 			if (bhash_is_valid(index)) {
 				BHASH_ASSERT(len_after == len_before - 1, "%s: %d -> %d", len_before, len_after);
-				BHASH_ASSERT(bhash_keys(tbl)[index] == key, "%s: %d -> %d", bhash_keys(tbl)[index], key);
-				BHASH_ASSERT(bhash_values(tbl)[index] == key * 2, "%s: %d -> %d", bhash_values(tbl)[index], key * 2);
+				BHASH_ASSERT(tbl.keys[index] == key, "%s: %d -> %d", tbl.keys[index], key);
+				BHASH_ASSERT(tbl.values[index] == key * 2, "%s: %d -> %d", tbl.values[index], key * 2);
 			} else {
 				BHASH_ASSERT(len_after == len_before, "%s: %d -> %d", len_before, len_after);
 			}
 
-			bhash_find(index, tbl, key, config);
+			index = bhash_find(&tbl, key);
 			assert(!bhash_is_valid(index));
-		} else if (action == BHASH_TEST_POP && bhash_len(tbl) > 0) {
-			bhash_index_t len_before = bhash_len(tbl);
-			int key = bhash_keys(tbl)[0];
+		} else if (action == BHASH_TEST_POP && bhash_len(&tbl) > 0) {
+			bhash_index_t len_before = bhash_len(&tbl);
+			int key = tbl.keys[0];
 			printf("Remove %d\n", key);
-			bhash_remove(index, tbl, key, config);
+			index = bhash_remove(&tbl, key);
 			memberships[key] = false;
-			bhash_index_t len_after = bhash_len(tbl);
+			bhash_index_t len_after = bhash_len(&tbl);
 
 			assert(bhash_is_valid(index));
 			BHASH_ASSERT(len_after == len_before - 1, "%s: %d -> %d", len_before, len_after);
-			BHASH_ASSERT(bhash_keys(tbl)[index] == key, "%s: %d -> %d", bhash_keys(tbl)[index], key);
-			BHASH_ASSERT(bhash_values(tbl)[index] == key * 2, "%s: %d -> %d", bhash_values(tbl)[index], key * 2);
+			BHASH_ASSERT(tbl.keys[index] == key, "%s: %d -> %d", tbl.keys[index], key);
+			BHASH_ASSERT(tbl.values[index] == key * 2, "%s: %d -> %d", tbl.values[index], key * 2);
 
-			bhash_find(index, tbl, key, config);
+			index = bhash_find(&tbl, key);
 			assert(!bhash_is_valid(index));
 		}
 
-		bhash_validate(tbl, config);
+		bhash_validate(&tbl);
 
 		int size = 0;
 		for (int i = 0; i < 10; ++i) {
 			bhash_index_t index;
-			bhash_find(index, tbl, i, config);
+			index = bhash_find(&tbl, i);
 			BHASH_ASSERT(
 				bhash_is_valid(index) == memberships[i],
 				"%s: Membership mismatch for %d",
@@ -103,12 +100,16 @@ int main(int argc, const char* argv[]) {
 			);
 
 			if (memberships[i]) { size += 1; }
+			if (bhash_is_valid(index) > 0) {
+				BHASH_ASSERT(tbl.keys[index] == i, "%s: Key mismatch: %d vs %d", tbl.keys[index], i);
+				BHASH_ASSERT(tbl.values[index] == i * 2, "%s: Value mismatch: %d vs %d", tbl.values[index], i * 2);
+			}
 		}
 
-		BHASH_ASSERT(size == bhash_len(tbl), "%s: Size mismatch: %d vs %d", size, bhash_len(tbl));
+		BHASH_ASSERT(size == bhash_len(&tbl), "%s: Size mismatch: %d vs %d", size, bhash_len(&tbl));
 	}
 
-	bhash_destroy(tbl, config);
+	bhash_cleanup(&tbl);
 
 	return 0;
 }
