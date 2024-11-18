ee2a6863e9aca20238577d83dd4d669bd5c1a835
Bach Le <bach@bullno1.com>
2024-11-18 15:08:41 +0800

    bhash: Change hash function to chibihash

--- bhash.h
+++ bhash.h
@@ -237,16 +237,76 @@ typedef struct {
 /*! Validate and assert when the table violates some invariants */
 #define bhash_validate(table) bhash__do_validate(&((table)->base))
 
+// small, fast 64 bit hash function.
+//
+// https://github.com/N-R-K/ChibiHash
+// https://nrk.neocities.org/articles/chibihash
+//
+// This is free and unencumbered software released into the public domain.
+// For more information, please refer to <https://unlicense.org/>
+static inline uint64_t
+bhash__chibihash64__load64le(const uint8_t *p) {
+	return (uint64_t)p[0] <<  0 | (uint64_t)p[1] <<  8 |
+	       (uint64_t)p[2] << 16 | (uint64_t)p[3] << 24 |
+	       (uint64_t)p[4] << 32 | (uint64_t)p[5] << 40 |
+	       (uint64_t)p[6] << 48 | (uint64_t)p[7] << 56;
+}
+
+static inline uint64_t
+bhash__chibihash64(const void *keyIn, ptrdiff_t len, uint64_t seed) {
+	const uint8_t *k = (const uint8_t *)keyIn;
+	ptrdiff_t l = len;
+
+	const uint64_t P1 = UINT64_C(0x2B7E151628AED2A5);
+	const uint64_t P2 = UINT64_C(0x9E3793492EEDC3F7);
+	const uint64_t P3 = UINT64_C(0x3243F6A8885A308C);
+
+	uint64_t h[4] = { P1, P2, P3, seed };
+
+	for (; l >= 32; l -= 32) {
+		for (int i = 0; i < 4; ++i, k += 8) {
+			uint64_t lane = bhash__chibihash64__load64le(k);
+			h[i] ^= lane;
+			h[i] *= P1;
+			h[(i+1)&3] ^= ((lane << 40) | (lane >> 24));
+		}
+	}
+
+	h[0] += ((uint64_t)len << 32) | ((uint64_t)len >> 32);
+	if (l & 1) {
+		h[0] ^= k[0];
+		--l, ++k;
+	}
+	h[0] *= P2; h[0] ^= h[0] >> 31;
+
+	for (int i = 1; l >= 8; l -= 8, k += 8, ++i) {
+		h[i] ^= bhash__chibihash64__load64le(k);
+		h[i] *= P2; h[i] ^= h[i] >> 31;
+	}
+
+	for (int i = 0; l > 0; l -= 2, k += 2, ++i) {
+		h[i] ^= (k[0] | ((uint64_t)k[1] << 8));
+		h[i] *= P3; h[i] ^= h[i] >> 31;
+	}
+
+	uint64_t x = seed;
+	x ^= h[0] * ((h[2] >> 32)|1);
+	x ^= h[1] * ((h[3] >> 32)|1);
+	x ^= h[2] * ((h[0] >> 32)|1);
+	x ^= h[3] * ((h[1] >> 32)|1);
+
+	// moremur: https://mostlymangling.blogspot.com/2019/12/stronger-better-morer-moremur-better.html
+	x ^= x >> 27; x *= UINT64_C(0x3C79AC492BA7B653);
+	x ^= x >> 33; x *= UINT64_C(0x1C69B3F74AC4AE35);
+	x ^= x >> 27;
+
+	return x;
+}
+
 // MurmurOAAT64
 static inline bhash_hash_t
 bhash_hash(const void* key, size_t size) {
-	bhash_hash_t h = 525201411107845655ull;
-	for (size_t i = 0; i < size; ++i) {
-		h ^= ((const unsigned char*)key)[i];
-		h *= 0x5bd1e9955bd1e995ull;
-		h ^= h >> 47;
-	}
-	return h;
+	return (bhash_hash_t)bhash__chibihash64(key, size, 0);
 }
 
 static inline bool
