ffe1930e78fcdfecb0b081daf16590988153520a
Bach Le <bach@bullno1.com>
2025-07-13 16:38:14 +0800

    autolist: Give more flexibility in namespacing entries

--- autolist.h
+++ autolist.h
@@ -4,17 +4,21 @@
 #include <stddef.h>
 
 #define AUTOLIST_ENTRY(LIST_NAME, ITEM_TYPE, ITEM_NAME) \
-	extern ITEM_TYPE ITEM_NAME; \
-	const autolist_entry_t autolist__##ITEM_NAME##_entry = { \
+	AUTOLIST_ENTRY_EX(LIST_NAME, ITEM_TYPE, ITEM_NAME, ITEM_NAME)
+
+#define AUTOLIST_ENTRY_EX(LIST_NAME, ITEM_TYPE, ITEM_NAME, VAR_NAME) \
+	extern ITEM_TYPE VAR_NAME; \
+	const autolist_entry_t AUTOLIST__CONCAT4(LIST_NAME, _, ITEM_NAME, _entry) = { \
 		.name = #ITEM_NAME, \
-		.name_length = sizeof(#ITEM_NAME) - 1, \
-		.value_addr = (void*)&ITEM_NAME, \
-		.value_size = sizeof(ITEM_NAME), \
+		.name_length = sizeof(AUTOLIST__STRINGIFY(ITEM_NAME)) - 1, \
+		.value_addr = (void*)&VAR_NAME, \
+		.value_size = sizeof(VAR_NAME), \
 	}; \
 	AUTOLIST__SECTION_BEGIN(LIST_NAME) \
-	const autolist_entry_t* const autolist__##ITEM_NAME##_info_ptr = &autolist__##ITEM_NAME##_entry; \
+	const autolist_entry_t* const AUTOLIST__CONCAT4(LIST_NAME, _, ITEM_NAME, _info_ptr) = \
+		&AUTOLIST__CONCAT4(LIST_NAME, _, ITEM_NAME, _entry); \
 	AUTOLIST__SECTION_END \
-	ITEM_TYPE ITEM_NAME
+	ITEM_TYPE VAR_NAME
 
 #define AUTOLIST_FOREACH(ITR, LIST_NAME) \
 	for ( \
@@ -24,19 +28,22 @@
 	) \
 		for (const autolist_entry_t* ITR = *autolist__itr; ITR != NULL; ITR = NULL)
 
+#define AUTOLIST__CONCAT3(A, B, C) AUTOLIST__CONCAT(AUTOLIST__CONCAT(A, B), C)
+#define AUTOLIST__CONCAT4(A, B, C, D) AUTOLIST__CONCAT(AUTOLIST__CONCAT(A, B), AUTOLIST__CONCAT(C, D))
+#define AUTOLIST__CONCAT(A, B) AUTOLIST__CONCAT_(A, B)
+#define AUTOLIST__CONCAT_(A, B) A##B
+#define AUTOLIST__STRINGIFY(X) AUTOLIST__STRINGIFY_(X)
+#define AUTOLIST__STRINGIFY_(X) #X
+
 #if defined(_MSC_VER)
-#	define AUTOLIST__CONCAT2(A, B) A##B
-#	define AUTOLIST__CONCAT(A, B) AUTOLIST__CONCAT2(A, B)
-#	define AUTOLIST__STR2(X) #X
-#	define AUTOLIST__STR(X) AUTOLIST__STR2(X)
 #	define AUTOLIST__SECTION_BEGIN(NAME) \
 	__pragma(data_seg(push)); \
-	__pragma(section(AUTOLIST__STR(AUTOLIST__CONCAT(NAME, $data)), read)); \
-	__declspec(allocate(AUTOLIST__STR(AUTOLIST__CONCAT(NAME, $data))))
+	__pragma(section(AUTOLIST__STRINGIFY(AUTOLIST__CONCAT(NAME, $data)), read)); \
+	__declspec(allocate(AUTOLIST__STRINGIFY(AUTOLIST__CONCAT(NAME, $data))))
 #elif defined(__APPLE__)
-#	define AUTOLIST__SECTION_BEGIN(NAME) __attribute__((retain, used, section("__DATA,autolist_" #NAME)))
+#	define AUTOLIST__SECTION_BEGIN(NAME) __attribute__((retain, used, section("__DATA,autolist_" AUTOLIST__STRINGIFY(NAME))))
 #elif defined(__unix__)
-#	define AUTOLIST__SECTION_BEGIN(NAME) __attribute__((retain, used, section("autolist_" #NAME)))
+#	define AUTOLIST__SECTION_BEGIN(NAME) __attribute__((retain, used, section("autolist_" AUTOLIST__STRINGIFY(NAME))))
 #else
 #	error Unsupported compiler
 #endif
@@ -58,26 +65,32 @@ typedef struct {
 
 #if defined(_MSC_VER)
 #	define AUTOLIST_DECLARE(NAME) \
-	extern const autolist_entry_t* const autolist_##NAME##_begin; \
-	extern const autolist_entry_t* const autolist_##NAME##_end;
+	extern const autolist_entry_t* const AUTOLIST__CONCAT3(autolist_, NAME, _begin); \
+	extern const autolist_entry_t* const AUTOLIST__CONCAT3(autolist_, NAME, _end);
 #	define AUTOLIST_IMPL(NAME) \
-	__pragma(section(AUTOLIST__STR(AUTOLIST__CONCAT(NAME, $begin)), read)); \
-	__pragma(section(AUTOLIST__STR(AUTOLIST__CONCAT(NAME, $data)), read)); \
-	__pragma(section(AUTOLIST__STR(AUTOLIST__CONCAT(NAME, $end)), read)); \
-	__declspec(allocate(AUTOLIST__STR(AUTOLIST__CONCAT(NAME, $begin)))) extern const autolist_entry_t* const autolist_##NAME##_begin = NULL; \
-	__declspec(allocate(AUTOLIST__STR(AUTOLIST__CONCAT(NAME, $end)))) extern const autolist_entry_t* const autolist_##NAME##_end = NULL;
+	__pragma(section(AUTOLIST__STRINGIFY(AUTOLIST__CONCAT(NAME, $begin)), read)); \
+	__pragma(section(AUTOLIST__STRINGIFY(AUTOLIST__CONCAT(NAME, $data)), read)); \
+	__pragma(section(AUTOLIST__STRINGIFY(AUTOLIST__CONCAT(NAME, $end)), read)); \
+	__declspec(allocate(AUTOLIST__STRINGIFY(AUTOLIST__CONCAT(NAME, $begin)))) \
+		extern const autolist_entry_t* const AUTOLIST__CONCAT3(autolist_, NAME, _begin) = NULL; \
+	__declspec(allocate(AUTOLIST__STRINGIFY(AUTOLIST__CONCAT(NAME, $end)))) \
+		extern const autolist_entry_t* const AUTOLIST__CONCAT3(autolist_, NAME, _end) = NULL;
 #elif defined(__APPLE__)
 #	define AUTOLIST_DECLARE(NAME) \
-	extern const autolist_entry_t* const __start_##NAME __asm("section$start$__DATA$autolist_" #NAME); \
-	extern const autolist_entry_t* const __stop_##NAME __asm("section$end$__DATA$autolist_" #NAME);
+	extern const autolist_entry_t* const AUTOLIST__CONCAT(__start_, NAME) \
+	__asm("section$start$__DATA$autolist_" AUTOLIST__STRINGIFY(NAME)); \
+	extern const autolist_entry_t* const AUTOLIST__CONCAT(__stop_, NAME) \
+	__asm("section$end$__DATA$autolist_" AUTOLIST__STRINGIFY(NAME));
 #	define AUTOLIST_IMPL(NAME) \
-	__attribute__((retain, used, section("__DATA,autolist_" #NAME))) const autolist_entry_t* const autolist_##NAME##__dummy = NULL;
+	__attribute__((retain, used, section("__DATA,autolist_" AUTOLIST__STRINGIFY(NAME)))) \
+		const autolist_entry_t* const AUTOLIST__CONCAT3(autolist_, NAME, __dummy) = NULL;
 #elif defined(__unix__)
 #	define AUTOLIST_DECLARE(NAME) \
-	extern const autolist_entry_t* const __start_autolist_##NAME; \
-	extern const autolist_entry_t* const __stop_autolist_##NAME;
+	extern const autolist_entry_t* const AUTOLIST__CONCAT(__start_autolist_, NAME); \
+	extern const autolist_entry_t* const AUTOLIST__CONCAT(__stop_autolist_, NAME);
 #	define AUTOLIST_IMPL(NAME) \
-	__attribute__((retain, used, section("autolist_" #NAME))) const autolist_entry_t* const autolist_##NAME##__dummy = NULL;
+	__attribute__((retain, used, section("autolist_" AUTOLIST__STRINGIFY(NAME)))) \
+		const autolist_entry_t* const AUTOLIST__CONCAT3(autolist_, NAME, __dummy) = NULL;
 #endif
 
 #define AUTOLIST_DEFINE(NAME) \
@@ -85,11 +98,11 @@ typedef struct {
 	AUTOLIST_IMPL(NAME)
 
 #if defined(_MSC_VER)
-#	define AUTOLIST_BEGIN(NAME) (&autolist_##NAME##_begin + 1)
-#	define AUTOLIST_END(NAME) (&autolist_##NAME##_end)
+#	define AUTOLIST_BEGIN(NAME) (&AUTOLIST__CONCAT3(autolist_, NAME, _begin) + 1)
+#	define AUTOLIST_END(NAME) (&AUTOLIST__CONCAT3(autolist_, NAME, _end))
 #elif defined(__unix__) || defined(__APPLE__)
-#	define AUTOLIST_BEGIN(NAME) (&__start_autolist_##NAME)
-#	define AUTOLIST_END(NAME) (&__stop_autolist_##NAME)
+#	define AUTOLIST_BEGIN(NAME) (&AUTOLIST__CONCAT(__start_autolist_, NAME))
+#	define AUTOLIST_END(NAME) (&AUTOLIST__CONCAT(__stop_autolist_, NAME))
 #endif
 
 #endif
