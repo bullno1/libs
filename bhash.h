56558db45d9d364899cbbad561a48a9838464978
Bach Le <bach@bullno1.com>
2025-12-27 08:23:56 +0800

    bhash: Update chibihash

--- bhash.h
+++ bhash.h
@@ -1,3 +1,4 @@
+// vim: set foldmethod=marker foldlevel=0:
 #ifndef BHASH_H
 #define BHASH_H
 
@@ -259,71 +260,87 @@ typedef struct {
 /*! Validate and assert when the table violates some invariants */
 #define bhash_validate(table) bhash__do_validate(&((table)->base))
 
-// small, fast 64 bit hash function.
+// chibihash {{{
+
+// small, fast 64 bit hash function (version 2).
 //
 // https://github.com/N-R-K/ChibiHash
-// https://nrk.neocities.org/articles/chibihash
 //
 // This is free and unencumbered software released into the public domain.
 // For more information, please refer to <https://unlicense.org/>
+
+static inline uint64_t
+bhash__chibihash64__load32le(const uint8_t *p) {
+	return (uint64_t)p[0] <<  0 | (uint64_t)p[1] <<  8 |
+	       (uint64_t)p[2] << 16 | (uint64_t)p[3] << 24;
+}
+
 static inline uint64_t
 bhash__chibihash64__load64le(const uint8_t *p) {
-	return (uint64_t)p[0] <<  0 | (uint64_t)p[1] <<  8 |
-	       (uint64_t)p[2] << 16 | (uint64_t)p[3] << 24 |
-	       (uint64_t)p[4] << 32 | (uint64_t)p[5] << 40 |
-	       (uint64_t)p[6] << 48 | (uint64_t)p[7] << 56;
+	return bhash__chibihash64__load32le(p) | (bhash__chibihash64__load32le(p+4) << 32);
+}
+
+static inline uint64_t
+bhash__chibihash64__rotl(uint64_t x, int n) {
+	return (x << n) | (x >> (-n & 63));
 }
 
 static inline uint64_t
 bhash__chibihash64(const void *keyIn, ptrdiff_t len, uint64_t seed) {
-	const uint8_t *k = (const uint8_t *)keyIn;
+	const uint8_t *p = (const uint8_t *)keyIn;
 	ptrdiff_t l = len;
 
-	const uint64_t P1 = UINT64_C(0x2B7E151628AED2A5);
-	const uint64_t P2 = UINT64_C(0x9E3793492EEDC3F7);
-	const uint64_t P3 = UINT64_C(0x3243F6A8885A308C);
+	const uint64_t K = UINT64_C(0x2B7E151628AED2A7); // digits of e
+	uint64_t seed2 = bhash__chibihash64__rotl(seed-K, 15) + bhash__chibihash64__rotl(seed-K, 47);
+	uint64_t h[4] = { seed, seed+K, seed2, seed2+(K*K^K) };
 
-	uint64_t h[4] = { P1, P2, P3, seed };
-
+	// depending on your system unrolling might (or might not) make things
+	// a tad bit faster on large strings. on my system, it actually makes
+	// things slower.
+	// generally speaking, the cost of bigger code size is usually not
+	// worth the trade-off since larger code-size will hinder inlinability
+	// but depending on your needs, you may want to uncomment the pragma
+	// below to unroll the loop.
+	//#pragma GCC unroll 2
 	for (; l >= 32; l -= 32) {
-		for (int i = 0; i < 4; ++i, k += 8) {
-			uint64_t lane = bhash__chibihash64__load64le(k);
-			h[i] ^= lane;
-			h[i] *= P1;
-			h[(i+1)&3] ^= ((lane << 40) | (lane >> 24));
+		for (int i = 0; i < 4; ++i, p += 8) {
+			uint64_t stripe = bhash__chibihash64__load64le(p);
+			h[i] = (stripe + h[i]) * K;
+			h[(i+1)&3] += bhash__chibihash64__rotl(stripe, 27);
 		}
 	}
 
-	h[0] += ((uint64_t)len << 32) | ((uint64_t)len >> 32);
-	if (l & 1) {
-		h[0] ^= k[0];
-		--l, ++k;
-	}
-	h[0] *= P2; h[0] ^= h[0] >> 31;
-
-	for (int i = 1; l >= 8; l -= 8, k += 8, ++i) {
-		h[i] ^= bhash__chibihash64__load64le(k);
-		h[i] *= P2; h[i] ^= h[i] >> 31;
+	for (; l >= 8; l -= 8, p += 8) {
+		h[0] ^= bhash__chibihash64__load32le(p+0); h[0] *= K;
+		h[1] ^= bhash__chibihash64__load32le(p+4); h[1] *= K;
 	}
 
-	for (int i = 0; l > 0; l -= 2, k += 2, ++i) {
-		h[i] ^= (k[0] | ((uint64_t)k[1] << 8));
-		h[i] *= P3; h[i] ^= h[i] >> 31;
+	if (l >= 4) {
+		h[2] ^= bhash__chibihash64__load32le(p);
+		h[3] ^= bhash__chibihash64__load32le(p + l - 4);
+	} else if (l > 0) {
+		h[2] ^= p[0];
+		h[3] ^= p[l/2] | ((uint64_t)p[l-1] << 8);
 	}
 
-	uint64_t x = seed;
-	x ^= h[0] * ((h[2] >> 32)|1);
-	x ^= h[1] * ((h[3] >> 32)|1);
-	x ^= h[2] * ((h[0] >> 32)|1);
-	x ^= h[3] * ((h[1] >> 32)|1);
+	h[0] += bhash__chibihash64__rotl(h[2] * K, 31) ^ (h[2] >> 31);
+	h[1] += bhash__chibihash64__rotl(h[3] * K, 31) ^ (h[3] >> 31);
+	h[0] *= K; h[0] ^= h[0] >> 31;
+	h[1] += h[0];
 
-	// moremur: https://mostlymangling.blogspot.com/2019/12/stronger-better-morer-moremur-better.html
-	x ^= x >> 27; x *= UINT64_C(0x3C79AC492BA7B653);
-	x ^= x >> 33; x *= UINT64_C(0x1C69B3F74AC4AE35);
-	x ^= x >> 27;
+	uint64_t x = (uint64_t)len * K;
+	x ^= bhash__chibihash64__rotl(x, 29);
+	x += seed;
+	x ^= h[1];
+
+	x ^= bhash__chibihash64__rotl(x, 15) ^ bhash__chibihash64__rotl(x, 42);
+	x *= K;
+	x ^= bhash__chibihash64__rotl(x, 13) ^ bhash__chibihash64__rotl(x, 31);
 
 	return x;
 }
+
+// }}}
 
 static inline bhash_hash_t
 bhash_hash(const void* key, size_t size) {
